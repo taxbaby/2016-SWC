93c5dc287fe5873e2ac6b1b62a058c921c2a9960
taxbaby <hopkins.e@husky.neu.edu>
2016-10-11 14:30:29 -0400

    make 33 non-intersecting lines grayscale

--- ps-04/b.js
+++ ps-04/b.js
@@ -1,11 +1,21 @@
 var canvasB = document.getElementById('canvasB');
 var drawingPad = canvasB.getContext('2d');
 
+var getColor = function(hue, saturation, lightness, alpha) {
+  var colorString = "hsla(" +
+    hue + ", "
+    + saturation + "%,"
+    + lightness  + "%,"
+    + alpha + ")";
+  return colorString;
+};
+
 
 var startX = 0;
 var startY = 0;
 var endX = 0;
 var endY = 0;
+var randomLightness = 0;
 
 for(var x = 0 ; x < 334 ; x++) {
 
@@ -17,6 +27,7 @@ for(var x = 0 ; x < 334 ; x++) {
     drawingPad.beginPath();
     drawingPad.moveTo(startX,startY);
     drawingPad.lineTo(endX, endY);
+    drawingPad.strokeStyle = getColor(0, 0, randomLightness, .6);
     drawingPad.stroke();
 
 }
