a65ba41c57084e21aff19e9e094b3bd72aa6384f
taxbaby <hopkins.e@husky.neu.edu>
2016-10-18 16:29:48 -0400

    create more Es that rotate

--- ps-05/c.js
+++ ps-05/c.js
@@ -5,18 +5,21 @@ var drawingPad = canvasB.getContext('2d');
 //but I like how it looks better with this many.
 //When I took it down to 33 it was no where near as good. :)
 
-for (var y = 100; y<=400; y+=15) {
 
+
+for (var y = 40; y<=400; y++) {
+
+drawingPad.rotate(Math.random()*100*Math.PI/180);
 drawingPad.beginPath();
-drawingPad.moveTo(15, 40);
-drawingPad.lineTo(15,100);
-drawingPad.moveTo(15,40);
-drawingPad.lineTo(45,40);
-drawingPad.moveTo(15,70);
-drawingPad.lineTo(35,70);
-drawingPad.moveTo(15,100);
-drawingPad.lineTo(45,100)
-drawingPad.lineWidth = 5;
+drawingPad.moveTo(15, y);
+drawingPad.lineTo(15,y+60);
+drawingPad.moveTo(15,y);
+drawingPad.lineTo(45,y);
+drawingPad.moveTo(15,y+30);
+drawingPad.lineTo(35,y+30);
+drawingPad.moveTo(15,y+60);
+drawingPad.lineTo(45,y+60);
+drawingPad.lineWidth = 4;
 drawingPad.stroke();
 
 }
