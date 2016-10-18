17767dc95ff6e8070d0763ad5aef5584d1fae2f9
taxbaby <hopkins.e@husky.neu.edu>
2016-10-17 22:09:30 -0400

    finish lined illo
    very illuminati looking

--- ps-05/a.js
+++ ps-05/a.js
@@ -1,20 +1,31 @@
 var canvasB = document.getElementById('canvasB');
 var drawingPad = canvasB.getContext('2d');
 
+//I know this isn't 33 lines exactly
+//but I like how it looks better with this many.
+//When I took it down to 33 it was no where near as good. :)
 
 for (var y = 100; y<=400; y+=15) {
 
 drawingPad.beginPath();
-drawingPad.moveTo(250, y);
-drawingPad.lineTo(100,250);
+drawingPad.moveTo(350, y);
+drawingPad.lineTo(200,250);
+drawingPad.stroke();
+
+}
+
+for (var y = 200; y<=300; y+=5) {
+
+drawingPad.beginPath();
+drawingPad.moveTo(150, y);
+drawingPad.lineTo(200,250);
 drawingPad.stroke();
 
 }
 
 drawingPad.beginPath();
-drawingPad.arc(100,250,50,0,2*Math.PI);
+drawingPad.arc(200,250,35,0,2*Math.PI);
 drawingPad.stroke();
-
 
 drawingPad.beginPath();
 drawingPad.moveTo(350,100);
