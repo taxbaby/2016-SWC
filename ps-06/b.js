9542df58124876230e11b35d0fe072a810134649
taxbaby <hopkins.e@husky.neu.edu>
2016-10-31 18:41:32 -0400

    finishing up

---
+++ ps-06/b.js
@@ -0,0 +1,13 @@
+ var addLine = function(event) {
+	event.preventDefault();
+	document.getElementById('newLine').value;
+	console.log(),val;
+	debugger
+}
+
+
+
+// var addLine = function() {
+//	document.getElementById('newLine').value;
+//	consol.log('newLine');
+//}
