b09bb5e2c4448aae89bcd8e143b2bbd07a33f446
taxbaby <hopkins.e@husky.neu.edu>
2016-10-31 21:12:54 -0400

    create initial function
    pink background with blue circles, onclick JS function, create Element
    div

--- ps-07/a.js
+++ ps-07/a.js
@@ -1,3 +1,10 @@
-var canvas =  document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+
+
+addEventListener("click",function(event) {
+	var dot = document.createElement("div");
+	dot.className="dot";
+	dot.style.left = (event.pageX - 40) + "px";
+    dot.style.top = (event.pageY - 40) + "px";
+    document.body.appendChild(dot);
+});
 
