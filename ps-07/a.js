a86567b6f28b7ffac9b5c59839e0c0e8ffac3f4a
taxbaby <hopkins.e@husky.neu.edu>
2016-10-31 21:34:47 -0400

    add triangle, square functions

--- ps-07/a.js
+++ ps-07/a.js
@@ -8,3 +8,18 @@ addEventListener("click",function(event) {
     document.body.appendChild(dot);
 });
 
+addEventListener("click",function(event) {
+	var square = document.createElement("div");
+	square.className="square";
+	square.style.left = (event.pageX - 40) + "px";
+    square.style.top = (event.pageY - 40) + "px";
+    document.body.appendChild(square);
+});
+
+addEventListener("click",function(event) {
+	var triangle = document.createElement("div");
+	triangle.className="triangle";
+	triangle.style.left = (event.pageX - 40) + "px";
+    triangle.style.top = (event.pageY - 40) + "px";
+    document.body.appendChild(triangle);
+});
