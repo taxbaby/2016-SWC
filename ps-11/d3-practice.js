afc4d17966f275eaa744259ec5120e758ed58a8b
taxbaby <hopkins.e@husky.neu.edu>
2016-11-28 18:56:42 -0500

    introduce JSON objects, plot racial data lines

---
+++ ps-11/d3-practice.js
@@ -0,0 +1,91 @@
+var height = 500;
+var width = 500;
+var margin = {left: 60, bottom: 60, right: 60, top: 60};
+
+
+//Scale.
+var yearScale     = d3.scaleLinear().domain([2012,1998]).range([height - margin.bottom, margin.top]);
+var percentScale  = d3.scaleLinear().domain([75,0]).range([margin.left, width - margin.right]);
+
+var svg = d3.select('svg');
+var axisBottom =d3.axisBottom(yearScale);
+var axisLeft =d3.axisLeft(percentScale);
+
+function yearToX1 (data) {
+ return yearScale(data.year1);
+};
+
+function yearToX2 (data) {
+ return yearScale(data.year2);
+};
+
+function percentToY1 (data) {
+	return percentScale(data.percent1)
+};
+
+function percentToY2 (data) {
+	return percentScale(data.percent2)
+};
+
+
+//Here is the data for the lines.
+//x axis is years
+//y axis is percent
+var jsonData = [
+	//Percent black population
+	{
+		"year1": 2000,
+		"percent1": 55,
+		"year2": 2010,
+		"percent2": 45,
+		"stroke": "hsl(0,40%,60%)"
+	},
+	//Percent white population
+	{
+		"year1": 2000,
+		"percent1": 26,
+		"year2": 2010,
+		"percent2": 33,
+		"stroke": "hsl(0,40%,60%)"	
+	}
+	//Percent rent burden
+	{}
+]
+
+var lines = svg.selectAll("lines")
+	.data(jsonData)
+	.enter()
+	.append("line");
+
+var lineAttributes = lines
+	.attr("x1", yearToX1)
+    .attr("y1", percentToY1)
+	.attr("x2", yearToX2)
+	.attr("y2", percentToY2)
+	.attr("stroke-width", 2)
+	.attr("stroke", function (d) { return d.stroke; });
+
+
+//The axes!
+svg.append('g')
+  .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+  .call(axisBottom);
+
+svg.append('g')
+  .attr("transform", "translate(" + margin.left + ",0)")
+  .call(axisLeft);
+
+// text label for the y axis
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", margin.left/3)
+  .attr("x", 0 - (height / 2))
+  .style("text-anchor", "middle")
+  .text("percent");
+
+// text label for the x axis
+svg.append("text")
+  .attr("y", height - margin.bottom/3)
+  .attr("x", width / 2)
+  .style("text-anchor", "middle")
+  .text("year");
---
+++ ps-11/d3-test.js
@@ -0,0 +1,117 @@
+var height = 500;
+var width = 500;
+var margin = {left: 60, bottom: 60, right: 60, top: 60};
+
+//Scales, etc, for the axes.
+
+var yearScale     = d3.scaleLinear().domain([2015,1995]).range([height - margin.bottom, margin.top]);
+var percentScale  = d3.scaleLinear().domain([100,0]).range([margin.left, width - margin.right]);
+
+var svg = d3.select('svg');
+var axisBottom =d3.axisBottom(yearScale);
+var axisLeft =d3.axisLeft(percentScale);
+
+//Plot data for black residents.
+
+var percentBlack = [
+  {year: 2000, percent: 55},
+  {year: 2010, percent: 45}
+];
+
+function percentToY (data) {
+ return percentScale(data.percent);
+};
+
+function yearToX (data) {
+  return yearScale(data.year);
+};
+
+
+svg.selectAll('circle')
+  .data(percentBlack)
+  .enter()
+  .append("circle")
+  .attr('class', 'weight')
+  .attr("cx", yearToX)
+  .attr("cy", percentToY)
+  .attr("fill", "hsl(0,40%,60%)")
+  .attr("r", '5');
+
+//Plot data for white residents.
+
+var percentWhite = [
+  {year: 2000, percent: 26},
+  {year: 2010, percent: 33}
+];
+
+function percentToY (data) {
+ return percentScale(data.percent);
+};
+
+function yearToX (data) {
+  return yearScale(data.year);
+};
+
+svg.selectAll('ellipse')
+  .data(percentWhite)
+  .enter()
+  .append("ellipse")
+  .attr('class', 'weight')
+  .attr("cx", yearToX)
+  .attr("cy", percentToY)
+  .attr("fill", "hsl(250,40%,60%)")
+  .attr("rx", '5')
+  .attr("ry",'5')
+
+//Rent burden
+
+var rentBurden = [
+  {year: 2000, percent: 36},
+  {year: 2010, percent: 50}
+];
+
+function percentToY (data) {
+ return percentScale(data.percent);
+};
+
+function yearToX (data) {
+  return yearScale(data.year);
+};
+
+svg.selectAll('rect')
+  .data(rentBurden)
+  .enter()
+  .append("rect")
+  .attr('class', 'weight')
+  .attr("x", yearToX)
+  .attr("y", percentToY)
+  .attr("fill", "hsl(300,40%,60%)")
+  .attr("width", '10')
+  .attr("height",'10')
+
+
+svg.append('g')
+  .attr("transform", "translate(0," + (height - margin.bottom) + ")")
+  .call(axisBottom);
+
+svg.append('g')
+  .attr("transform", "translate(" + margin.left + ",0)")
+  .call(axisLeft);
+
+
+
+
+// text label for the y axis
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", margin.left/3)
+  .attr("x", 0 - (height / 2))
+  .style("text-anchor", "middle")
+  .text("percent");
+
+// text label for the x axis
+svg.append("text")
+  .attr("y", height - margin.bottom/3)
+  .attr("x", width / 2)
+  .style("text-anchor", "middle")
+  .text("year");
