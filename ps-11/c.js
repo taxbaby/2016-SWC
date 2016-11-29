682664114229b905663663573de0ce1a316eefff
taxbaby <hopkins.e@husky.neu.edu>
2016-11-29 14:17:26 -0500

    add clock files

---
+++ ps-11/c.js
@@ -0,0 +1,72 @@
+var height = 300;
+var width = 300;
+var timeData = [ 
+  { value: 0, name: "hour"},
+  { value: 0, name: "minute"},
+  { value: 0, name: "seconds"}
+  ]
+
+var yHourScale = d3.scaleLinear()
+  .domain([1,24])
+  .range([height,0]);
+
+var yScale = d3.scaleLinear()
+  .domain([1,60])
+  .range([height,0]);
+
+function updateTimeData() {
+  var d = new Date();
+  var hour = d.getHours();
+  var minute = d.getMinutes();
+  var second = d.getSeconds();
+  timeData[0].value = hour;
+  timeData[1].value = minute;
+  timeData[2].value = second;
+  updateSVG(timeData)
+};
+
+setInterval(updateTimeData,1000);
+
+var div = d3.select('div');
+var firstClock = div.append('svg').attr('width',300).attr('height',300);
+var secondClock = div.append('svg').attr('width',300).attr('height',300);
+
+var circles = firstClock.selectAll('circle')
+  .data(timeData)
+  .enter()
+  .append('circle')
+  .attr('r', 5)
+  .style('fill', 'black');
+
+var rectangles = secondClock.selectAll('rect')
+  .data(timeData)
+  .enter()
+  .append('rect')
+  .attr('width', 5)
+  .style('fill', 'black');
+
+
+function updateSVG(newData) {
+  circles
+    .attr('cx', function(datum, i) { return i * 100 + 50; })
+    .attr('cy', function(datum, i) { 
+      var scale = yScale;
+      if(datum.name == "hour"){
+        scale = yHourScale;
+      }
+      return scale(datum.value);  
+  });
+  rectangles
+    .attr('x', function(datum, i) { return i * 100 + 50; })
+    .attr('height', function(datum, i) {
+      var scale = yScale;
+      if(datum.name == "hour"){
+        scale = yHourScale;
+      }
+      return scale(datum.value);  
+     })
+    .attr('y', function(datum, i) {  
+      return 0;
+    });
+}
+
