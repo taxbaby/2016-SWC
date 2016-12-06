98fbc57876f5c8542273dfb7498b83b6e37c386c
taxbaby <hopkins.e@husky.neu.edu>
2016-12-05 19:53:58 -0500

    more set up

--- final/js/index.js
+++ final/js/index.js
@@ -1,10 +1,36 @@
 (function() {
 
-  // Fake JSON data
-  var json = {"countries_msg_vol": {
-    "CA": 170, "US": 393, "BB": 12, "CU": 9, "BR": 89, "MX": 192, "PY": 32, "UY": 9, "VE": 25, "BG": 42, "CZ": 12, "HU": 7, "RU": 184, "FI": 42, "GB": 162, "IT": 87, "ES": 65, "FR": 42, "DE": 102, "NL": 12, "CN": 92, "JP": 65, "KR": 87, "TW": 9, "IN": 98, "SG": 32, "ID": 4, "MY": 7, "VN": 8, "AU": 129, "NZ": 65, "GU": 11, "EG": 18, "LY": 4, "ZA": 76, "A1": 2, "Other": 254 
-  }};
-  
+  //Census data
+var json = {
+  '2000': {
+    'population': {
+      'total': 21284,
+      'race': {
+        'black': 11597,
+        'white': 5468,
+        'latinx': 4336,
+        'asian': 503,
+        'nativeAmerican': 204,
+        'nativePacific': 0,
+        'other': 2251,
+      }
+    }
+  },
+  '2010': {
+    'population': {
+      'total': 22287,
+      'race': {
+        'black': 10095,
+        'white': 7411,
+        'latinx': 5386,
+        'asian': 1140,
+        'nativeAmerican': 152,
+        'nativePacific': 6,
+        'other': 2388
+      }
+    }
+  }
+};
 	// D3 Bubble Chart 
 
 	var diameter = 600;
@@ -22,8 +48,8 @@
 				.padding(3);
   
   // generate data with calculated layout values
-  var nodes = bubble.nodes(processData(json))
-						.filter(function(d) { return !d.children; }); // filter out the outer bubble
+  var nodes = bubble.nodes(processData(json));
+						//.filter(function(d) { return !d.children; }); // filter out the outer bubble
  
   var vis = svg.selectAll('circle')
 					.data(nodes);
@@ -34,7 +60,7 @@
 			.attr('class', function(d) { return d.className; });
   
   function processData(data) {
-    var obj = data.countries_msg_vol;
+    var obj = data.population;
 
     var newDataSet = [];
 
