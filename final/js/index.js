1505106742bc0c00a2d38f8cc651d15098c85c5e
taxbaby <hopkins.e@husky.neu.edu>
2016-12-05 22:12:53 -0500

    modify viz to display Roxbury data; add some styling, flexbox

--- final/js/index.js
+++ final/js/index.js
@@ -4,28 +4,28 @@
 var json = {
   '2000': {
     'population': {
-      'total': 21284,
+      //'total': 21284,
       'race': {
         'black': 11597,
         'white': 5468,
         'latinx': 4336,
         'asian': 503,
-        'nativeAmerican': 204,
-        'nativePacific': 0,
+        'nativeamerican': 204,
+        //'nativePacific': 0,
         'other': 2251,
       }
     }
   },
   '2010': {
     'population': {
-      'total': 22287,
+      //'total': 22287,
       'race': {
         'black': 10095,
         'white': 7411,
         'latinx': 5386,
         'asian': 1140,
-        'nativeAmerican': 152,
-        'nativePacific': 6,
+        'nativeamerican': 152,
+        'nativepacific': 6,
         'other': 2388
       }
     }
@@ -48,19 +48,28 @@ var json = {
 				.padding(3);
   
   // generate data with calculated layout values
-  var nodes = bubble.nodes(processData(json));
-						//.filter(function(d) { return !d.children; }); // filter out the outer bubble
+  var nodes = bubble.nodes(processData(json))
+						.filter(function(d) { return !d.children; }); // filter out the outer bubble
  
   var vis = svg.selectAll('circle')
 					.data(nodes);
+  
+  var label = svg.selectAll('text')
+          .data(nodes);
   
   vis.enter().append('circle')
 			.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
 			.attr('r', function(d) { return d.r; })
 			.attr('class', function(d) { return d.className; });
   
+  vis.enter().append('text')
+      .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+      .text( function (d) { return d.className })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "14px");
+
   function processData(data) {
-    var obj = data.population;
+    var obj = data['2000'].population.race;
 
     var newDataSet = [];
 
@@ -71,3 +80,6 @@ var json = {
   }
   
 })();
+
+//https://www.pubnub.com/blog/2014-10-08-fun-with-d3js-data-visualization-eye-candy-with-streaming-json/
+//http://bl.ocks.org/mmattozzi/7018021
